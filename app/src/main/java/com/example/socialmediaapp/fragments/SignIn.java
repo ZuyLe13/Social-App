59f74ef9176e1f7b6345f4839e8001951a4ce312
Dile <duyle131103@gmail.com>
2024-04-18 00:49:24 +0700

    Init project

---
+++ app/src/main/java/com/example/socialmediaapp/fragments/SignIn.java
@@ -0,0 +1,25 @@
+package com.example.socialmediaapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.socialmediaapp.R;
+
+public class SignIn extends Fragment {
+
+    public SignIn() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+    }
+}
