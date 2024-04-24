6db1e0eb47f251054650e77bba790a94d0383328
Dile <duyle131103@gmail.com>
2024-04-24 13:31:28 +0700

    Fix name file

--- app/src/main/java/com/example/socialmediaapp/fragments/SignIn.java
+++ app/src/main/java/com/example/socialmediaapp/fragments/SignIn.java
@@ -2,19 +2,18 @@ package com.example.socialmediaapp.fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.socialmediaapp.R;
 
 public class SignIn extends Fragment {
-
-    public SignIn() {
-        // Required empty public constructor
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
