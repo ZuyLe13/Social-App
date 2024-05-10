82c519719368a0dedd63b52511ef84433a5bd4c8
Nnhatphuonghuy <21522156@gm.uit.edu.vn>
2024-05-10 23:13:00 +0700

    FE add 2 Edit_Profile và list_edit_profile Collection, fix others_user_profile, ...

---
+++ app/src/main/java/com/example/socialmediaapp/fragments/RecyclerViewCollection.java
@@ -0,0 +1,23 @@
+package com.example.socialmediaapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.socialmediaapp.R;
+
+public class RecyclerViewCollection extends Fragment
+{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recyclerview_collection, container, false);
+
+    }
+
+
+}
+
